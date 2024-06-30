c9d0c7385929bd0511e921764b9ebfa1c3f8bd16
Cristian David Home <cristian_david033@hotmail.com>
2024-06-30 11:05:58 -0500

    Nueva compilación

--- assets/404NotFound-B1CKposk.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o,c}from"./index-DVss1fbH.js";const n={};function t(r,_){return o(),c("h1",null,"Page Not Found")}const s=e(n,[["render",t]]);export{s as default};
---
+++ assets/ResultsView-Htxw7Pc5.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as s}from"./index-D1hjjF-z.js";const t={};function n(o,r){return c(),s("h1",null,"Results View")}const a=e(t,[["render",n]]);export{a as default};
